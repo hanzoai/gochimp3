be1ebd963bd665d7fecc9a4b8f75f0996284f2ec
Zach Kelling <z@zeekay.io>
2016-09-01 01:03:25 -0500

    Add basic test around serialization hack.

--- json.go
+++ json.go
@@ -5,28 +5,30 @@ import (
 	"time"
 )
 
-func (order Order) MarshalJSON() ([]byte, error) {
+func (order *Order) MarshalJSON() ([]byte, error) {
 	tmp := struct {
-		*Order
+		Order
 		ProcessedAtForeign string `json:"processed_at_foreign"`
 		CancelledAtForeign string `json:"cancelled_at_foreign"`
 		UpdatedAtForeign   string `json:"updated_at_foreign"`
 	}{
-		Order:              &order,
+		Order:              *order,
 		ProcessedAtForeign: order.ProcessedAtForeign.Format(time.RFC3339),
 		CancelledAtForeign: order.CancelledAtForeign.Format(time.RFC3339),
 		UpdatedAtForeign:   order.UpdatedAtForeign.Format(time.RFC3339),
 	}
+
 	return json.Marshal(tmp)
 }
 
-func (product Product) MarshalJSON() ([]byte, error) {
+func (product *Product) MarshalJSON() ([]byte, error) {
 	tmp := struct {
-		*Product
+		Product
 		PublishedAtForeign string `json:"published_at_foreign"`
 	}{
-		Product:            &product,
+		Product:            *product,
 		PublishedAtForeign: product.PublishedAtForeign.Format(time.RFC3339),
 	}
+
 	return json.Marshal(tmp)
 }
