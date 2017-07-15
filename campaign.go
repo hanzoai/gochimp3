191975214f3f94b198f0322afc5c2c334e0e939e
boosh <boosh@example.com>
2017-07-15 08:10:45 +0100

    Add basic support for campaigns

---
+++ campaign.go
@@ -0,0 +1,190 @@
+package gochimp3
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	campaigns_path       = "/campaigns"
+	single_campaign_path = campaigns_path + "/%s"
+)
+
+
+type CampaignQueryParams struct {
+	ExtendedQueryParams
+
+	Type string
+	Status string
+	BeforeSendTime string
+	SinceSendTime string
+	BeforeCreateTime string
+	SinceCreateTime string
+	ListId string
+	FolderId string
+	SortField string
+	SortDir string
+}
+
+func (q CampaignQueryParams) Params() map[string]string {
+	m := q.ExtendedQueryParams.Params()
+	m["type"] = q.Type
+	m["status"] = q.Status
+	m["before_send_time"] = q.BeforeSendTime
+	m["since_send_time"] = q.SinceSendTime
+	m["before_create_time"] = q.BeforeCreateTime
+	m["since_create_time"] = q.SinceCreateTime
+	m["list_id"] = q.ListId
+	m["folder_id"] = q.FolderId
+	m["sort_field"] = q.SortField
+	m["sort_dir"] = q.SortDir
+	return m
+}
+
+type ListOfCampaigns struct {
+	baseList
+	Campaigns []CampaignResponse `json:"campaigns"`
+}
+
+type CampaignCreationRequest struct {
+	Name                string           `json:"name"`
+	//Contact             Contact          `json:"contact"`
+	//PermissionReminder  string           `json:"permission_reminder"`
+	//UseArchiveBar       bool             `json:"use_archive_bar"`
+	//CampaignDefaults    CampaignDefaults `json:"campaign_defaults"`
+	//NotifyOnSubscribe   string           `json:"notify_on_subscribe"`
+	//NotifyOnUnsubscribe string           `json:"notify_on_unsubscribe"`
+	//EmailTypeOption     bool             `json:"email_type_option"`
+	//Visibility          string           `json:"visibility"`
+}
+
+type CampaignRecipients struct {
+	ListId string `json:"list_id"`
+	ListName string `json:"list_name"`
+	SegmentText string `json:"segment_text"`
+	RecipientCount int `json:"recipient_count"`
+}
+
+type CampaignSettings struct {
+	SubjectLine     string `json:"subject_line"`
+	PreviewText     string `json:"preview_text"`
+	Title           string `json:"title"`
+	FromName        string `json:"from_name"`
+	ReplyTo         string `json:"reply_to"`
+	UseConversation bool `json:"use_conversation"`
+	ToName          string `json:"to_name"`
+	FolderId        string `json:"folder_id"`
+	Authenticate    bool `json:"authenticate"`
+	AutoFooter      bool `json:"auto_footer"`
+	InlineCss       bool `json:"inline_css"`
+	AutoTweet       bool `json:"auto_tweet"`
+	FbComments      bool `json:"fb_comments"`
+	Timewarp        bool `json:"timewarp"`
+	TemplateId      uint `json:"template_id"`
+	DragAndDrop     bool `json:"drag_and_drop"`
+}
+
+type CampaignTracking struct {
+	Opens           bool `json:"opens"`
+	HtmlClicks      bool `json:"html_clicks"`
+	TextClicks      bool `json:"text_clicks"`
+	GoalTracking    bool `json:"goal_tracking"`
+	Ecomm360        bool `json:"ecomm360"`
+	GoogleAnalytics string `json:"google_analytics"`
+	Clicktale       string `json:"clicktale"`
+}
+
+type CampaignEcommerce struct {
+	TotalOrders  int `json:"total_orders"`
+	TotalSpent   int `json:"total_spent"`
+	TotalRevenue int `json:"total_revenue"`
+}
+
+type CampaignReportSummary struct {
+	Opens            int `json:"opens"`
+	UniqueOpens      int `json:"unique_opens"`
+	OpenRate         float32 `json:"open_rate"`
+	Clicks           int `json:"clicks"`
+	SubscriberClicks int `json:"subscriber_clicks"`
+	ClickRate        float32 `json:"click_rate"`
+	Ecommerce        CampaignEcommerce `json:"ecommerce"`
+}
+
+type CampaignDeliveryStatus struct {
+	Enabled  		 bool `json:"enabled"`
+}
+
+type CampaignResponse struct {
+	withLinks
+
+	ID                string `json:"id"`
+	WebID             uint   `json:"web_id"`
+	Type              string `json:"type"`
+	CreateTime        string `json:"create_time"`
+	ArchiveUrl        string `json:"archive_url"`
+	LongArchiveUrl    string `json:"long_archive_url"`
+	Status            string `json:"status"`
+	EmailsSent        uint   `json:"emails_sent"`
+	SendTime          string `json:"send_time"`
+	ContentType       string `json:"content_type"`
+	NeedsBlockRefresh bool   `json:"needs_block_refresh"`
+	Recipients        CampaignRecipients `json:"recipients"`
+	Settings          CampaignSettings `json:"settings"`
+	Tracking          CampaignTracking `json:"tracking"`
+	ReportSummary     CampaignReportSummary `json:"report_summary"`
+	DeliveryStatus    CampaignDeliveryStatus `json:"delivery_status"`
+
+	api *API
+}
+
+func (campaign CampaignResponse) CanMakeRequest() error {
+	if campaign.ID == "" {
+		return errors.New("No ID provided on campaign")
+	}
+
+	return nil
+}
+
+func (api API) GetCampaigns(params *CampaignQueryParams) (*ListOfCampaigns, error) {
+	response := new(ListOfCampaigns)
+
+	err := api.Request("GET", campaigns_path, params, nil, response)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, l := range response.Campaigns {
+		l.api = &api
+	}
+
+	return response, nil
+}
+
+func (api API) GetCampaign(id string, params *BasicQueryParams) (*CampaignResponse, error) {
+	endpoint := fmt.Sprintf(single_campaign_path, id)
+
+	response := new(CampaignResponse)
+	response.api = &api
+
+	return response, api.Request("GET", endpoint, params, nil, response)
+}
+
+func (api API) CreateCampaign(body *CampaignCreationRequest) (*CampaignResponse, error) {
+	response := new(CampaignResponse)
+	response.api = &api
+	return response, api.Request("POST", campaigns_path, nil, body, response)
+}
+
+func (api API) UpdateCampaign(id string, body *CampaignCreationRequest) (*CampaignResponse, error) {
+	endpoint := fmt.Sprintf(single_campaign_path, id)
+
+	response := new(CampaignResponse)
+	response.api = &api
+
+	return response, api.Request("PATCH", endpoint, nil, body, response)
+}
+
+func (api API) DeleteCampaign(id string) (bool, error) {
+	endpoint := fmt.Sprintf(single_campaign_path, id)
+	return api.RequestOk("DELETE", endpoint)
+}
