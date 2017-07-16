9ef00e6461f03dc443ee33d7a22a2f6d4d858e3b
boosh <boosh@example.com>
2017-07-16 07:59:30 +0100

    Can now get template folders and create templates

---
+++ template_folders.go
@@ -0,0 +1,40 @@
+package gochimp3
+
+const (
+	template_folders_path       = "/template-folders"
+	// single folder endpoint not implemented
+)
+
+type TemplateFolderQueryParams struct {
+	ExtendedQueryParams
+}
+
+type ListOfTemplateFolders struct {
+	baseList
+	Folders []TemplateFolder `json:"folders"`
+}
+
+type TemplateFolder struct {
+	withLinks
+
+	Name  string `json:"name"`
+	ID    string `json:"id"`
+	Count uint `json:"count"`
+
+	api *API
+}
+
+func (api API) GetTemplateFolders(params *TemplateFolderQueryParams) (*ListOfTemplateFolders, error) {
+	response := new(ListOfTemplateFolders)
+
+	err := api.Request("GET", template_folders_path, params, nil, response)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, l := range response.Folders {
+		l.api = &api
+	}
+
+	return response, nil
+}
